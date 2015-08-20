86eecc8deea25ea446c41b038d3ad3aa52536efd
ua <ulf.angermann@innoq.com>
2015-08-20 09:37:25 +0200

    fixed the tests

--- src/janga_config.erl
+++ src/janga_config.erl
@@ -16,7 +16,6 @@
 %% --------------------------------------------------------------------
 -export([get_module_config/1, set_module_config/2]).
 -export([get_value/2, get_value/3, get_values/2, get_level_values/3]).
--export([set_value_data/3]).
 -export([get_ports/0, get_port/1, get_env/2]).
 %% --------------------------------------------------------------------
 %% record definitions
@@ -29,16 +28,9 @@ get_module_config(Config) ->
     {driver, Module, Module_config} = lists:keyfind(driver, 1, Config),
     Module_config.
 
-set_value_data(Key, Value, Config) -> 
-    MC = get_module_config(Config),
-    D = get_value(data, MC, []),    
-    D1 = lists:keystore(Key, 1, D, {Key, Value}),
-    MC_1 = lists:keystore(data, 1, MC, {data, D1}),
-    set_module_config(Config, MC_1).
-
 set_module_config(Config, Module_config) ->
-    {driver, {Module, Func}, _Module_config} = lists:keyfind(driver, 1, Config),
-    lists:keyreplace(driver, 1, Config, {driver, {Module, Func}, Module_config}).
+    {driver, Module, _Module_config} = lists:keyfind(driver, 1, Config),
+    lists:keyreplace(driver, 1, Config, {driver, Module, Module_config}).
 
 get_level_values(Level, Keys, List_of_tuples) ->
     List_of_tuples_1 = get_level(Level, List_of_tuples),
@@ -58,9 +50,14 @@ get_values([Key|Keys], List_of_tuples, Acc) when is_list(List_of_tuples) ->
     get_values(Keys, List_of_tuples, [get_value(Key, List_of_tuples)|Acc]).
 
 get_value(Key, List_of_tuples) when is_list(List_of_tuples)  ->
-    proplists:get_value(Key, List_of_tuples).
+    get_value(proplists:lookup(Key, List_of_tuples)).
 get_value(Key, List_of_tuples, Default) when is_list(List_of_tuples)  ->
-    proplists:get_value(Key, List_of_tuples, Default).
+    get_value(proplists:lookup(Key, List_of_tuples, Default)).
+
+get_value({K, V}) ->
+  V;
+get_value(none) ->
+  [].
 
 get_port(JApp) ->
   Ports = get_env(janga_core, ports),  
@@ -78,7 +75,16 @@ get_env(Application, Key) ->
 -include_lib("eunit/include/eunit.hrl").
 -ifdef(TEST).
 
+
+get_value_test() ->
+    List_of_tuples = [{key1, "V1"}, {key2, "v2"}, {key3, [{sub3_k1, "v31"}, {sub3_k2, "v32"}]}, {key4, "v4"}],
+    ?assertEqual([{sub3_k1,"v31"},{sub3_k2,"v32"}], get_value(key3, List_of_tuples)).
+
 get_level_test() ->
+    List_of_tuples = [{key1, "V1"}, {key2, "v2"}, {key3, [{sub3_k1, "v31"}, {sub3_k2, "v32"}]}, {key4, "v4"}],
+    ?assertEqual([{sub3_k1, "v31"}, {sub3_k2, "v32"}], get_level([key3], List_of_tuples)).  
+
+get_level_values_test() ->
     List_of_tuples = [{key1, "V1"}, {key2, "v2"}, {key3, [{sub3_k1, "v31"}, {sub3_k2, "v32"}]}, {key4, "v4"}],
     ?assertEqual(["v31"], get_level_values([key3], [sub3_k1], List_of_tuples)).
 
@@ -108,23 +114,5 @@ get_module_config_test() ->
             {temp_min,"16.0"},
             {temp_max,"21.0"}]}],
 
-    ?assertEqual(Result, get_module_config(Config)).
-
-set_value_data_test() ->
-    Config = 
-     [{id, "0"},
-      {activ, false},
-     {driver, {sensortag_driver,handle_msg},
-        [{data,[]}]}],
-
-    Result = 
-     [{id, "0"},
-      {activ, false},
-     {driver, {sensortag_driver,handle_msg},
-        [{data,[{port, "test"}]}]}],
-
-    ?assertEqual(Result, set_value_data(port, "test", Config)).
-
-
-    
+    ?assertEqual(Result, get_module_config(Config)).    
 -endif.
