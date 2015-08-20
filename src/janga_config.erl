f7aa39d07201ac45081054f2e6b4290fdeabe6ee
ua <ulf.angermann@innoq.com>
2015-08-20 09:59:10 +0200

    fixed config

--- src/janga_config.erl
+++ src/janga_config.erl
@@ -54,6 +54,8 @@ get_value(Key, List_of_tuples) when is_list(List_of_tuples)  ->
 get_value(Key, List_of_tuples, Default) when is_list(List_of_tuples)  ->
     get_value(proplists:lookup(Key, List_of_tuples, Default)).
 
+get_value({K, V1, V2}) ->
+  V2.
 get_value({K, V}) ->
   V;
 get_value(none) ->
