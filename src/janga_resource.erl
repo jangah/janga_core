5b85ed6d5c507c392be0a42e084b266a95c79853
ua <ulf.angermann@innoq.com>
2015-08-28 09:23:53 +0200

    added modul for convience resource handling

---
+++ src/janga_resource.erl
@@ -0,0 +1,52 @@
+%%
+%% Copyright (c) 2013 Ulf Angermann  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+
+%%% -------------------------------------------------------------------
+%%% Author  : Ulf uaforum1@googlemail.com
+%%% Description :
+%%%
+%%% Created : 
+%%% -------------------------------------------------------------------
+-module(janga_resource).
+
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+%% --------------------------------------------------------------------
+%% External exports
+%% --------------------------------------------------------------------
+-export([get_qs_value/3]).
+
+get_qs_value(Key, ReqData, Default) ->
+  case wrq:get_qs_value(Key, ReqData) of 
+    undefined -> Default;
+    Any -> Any
+  end.
+%% --------------------------------------------------------------------
+%% record definitions
+%% --------------------------------------------------------------------
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
+
+%% --------------------------------------------------------------------
+%%% Test functions
+%% --------------------------------------------------------------------
+-include_lib("eunit/include/eunit.hrl").
+-ifdef(TEST).
+-endif.
