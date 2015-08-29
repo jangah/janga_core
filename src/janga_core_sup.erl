16b05a041462eb0b8618a4717e0ade2240871389
ua <ulf.angermann@innoq.com>
2015-08-29 07:03:42 +0200

    added server sent event functionality

--- src/janga_core_sup.erl
+++ src/janga_core_sup.erl
@@ -10,7 +10,7 @@
 
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
-
+-define(CHILD_ARG_1(I, Type, Arg1), {Arg1, {I, start_link, [Arg1]}, temporary, 5000, Type, [I]}).
 %% ===================================================================
 %% API functions
 %% ===================================================================
@@ -28,7 +28,8 @@ init([]) ->
     	?CHILD(janga_ets_mgr, worker),    	
     	?CHILD(janga_actor_group, worker),
     	?CHILD(actor_group, worker),
-    	?CHILD(janga_account_sup, supervisor)
+    	?CHILD(janga_account_sup, supervisor),
+    	?CHILD_ARG_1(janga_service_event, worker, "thing_event_manager")
     	]
     	}}.
 
