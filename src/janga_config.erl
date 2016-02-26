1f49fed818bff57ed5fff6b906d30506677e7172
ua <ulf.angermann@innoq.com>
2016-02-26 06:03:25 +0100

    added function to get the repo path

--- src/janga_config.erl
+++ src/janga_config.erl
@@ -19,7 +19,7 @@
 -export([get_value/2, get_value/3, get_values/2, get_level_values/3]).
 -export([get_ports/1, get_port/1, get_env/2]).
 -export([get_service_config/1, get_name/1, get_notify/0]).
--export([get_name/0, get_repo_uri/0]).
+-export([get_name/0, get_repo_uri/0, get_repo_dir/0]).
 %% --------------------------------------------------------------------
 %% record definitions
 %% --------------------------------------------------------------------
@@ -93,6 +93,8 @@ is_application_loaded(Japp, Apps) ->
 get_repo_uri() ->
   get_env(janga_core, repo_uri).
 
+get_repo_dir() ->
+  get_env(janga_core, repo_dir).
 
 get_name() ->
   get_env(janga_core, name).
