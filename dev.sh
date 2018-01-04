502b98151f8935fc0ec1e87ce7e6688f3116195b
Ulf Angermann <uangermann@gmail.com>
2018-01-04 08:36:45 +0100

    refactored the dependencies

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p log
-erl -sname jangah -setcookie nocookie -pa $PWD/ebin $PWD/_build/default/lib/*/ebin -boot start_sasl -s janga_core -s observer -config ../janga/etc/app.config
+erl -sname jangah -setcookie nocookie -pa $PWD/_build/default/lib/*/ebin -boot start_sasl -s janga_core -s observer -config ../janga/etc/app.config
